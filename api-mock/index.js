5b34d3a0f40b8694b43ac843963ecca43e028e66
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2016-12-14 20:24:29 -0200

    feat(api-mock): add mock data endpoints
    Add endpoint for impressions
    Add endpoint for revenue
    Add endpoint for visits

--- api-mock/index.js
+++ api-mock/index.js
@@ -2,6 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.get('/revenue', function(req, res) {
+  res.json({
+    total: 20000,
+    smartphone: 80000,
+    tablet: 120000
+  });
+});
+
+app.get('/impressions', function(req, res) {
+  res.json({
+    total: 50000000,
+    smartphone: 30000000,
+    tablet: 20000000
+  });
+});
+
+app.get('/visits', function(req, res) {
+  res.json({
+    total: 60000000,
+    smartphone: 120000000,
+    tablet: 480000000
+  });
+});
+
 app.get('/health', function(req, res) {
   res.json({ok: true});
 });
